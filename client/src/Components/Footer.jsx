076a9d918062c6294ea491557764a9f9355631b9
Keya Dey <keya.dey22@spit.ac.in>
2025-07-03 21:20:23 +0530

    Initial commit by Keya Dey

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Box, Heading, Center } from '@chakra-ui/react';
+
+function Footer() {
+  return (
+    <Box padding={8}>
+     
+    </Box>
+  );
+}
+
+export default Footer;
